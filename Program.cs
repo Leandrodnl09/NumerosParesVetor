7b2d33a2ae0ce42a52216a9c2f7d4b0b04f71e7c
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-04 19:55:25 -0300

    NumerosParesVetor

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Exercício curso Nélio Alves "Numeros Pares Vetor"
+// Criar um vetor de numeros aleatorio, depois mostrar na tela es números pares e a quantidade de numeros pares.
+
+int N = int.Parse(Console.ReadLine());
+int [] Num = new int[N];
+string[] X = Console.ReadLine().Split(' ');
+
+// Comando para leitura de números na mesma linha;
+for (int i = 0; i < N; i++)
+{
+    Num[i] = int.Parse(X[i]);
+}
+// Comando para mostrar na tela apenas os números pares;
+for (int i = 0; i < N; i ++)
+{
+    if (Num[i] % 2 == 0)
+    Console.Write(Num[i] + " ");
+}
+// Coamndo para pazer a contagem dos números pares;
+int Cont = 0;
+for (int i = 0; i < N; i ++)
+{
+    if (Num[i] % 2 == 0)
+    {
+        Cont ++;
+    }  
+}
+Console.WriteLine("\n" + Cont);
